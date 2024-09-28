4d28f05100d12dd5198b20b1c325f69e4d2541b7
Divyanshu Salwan <114092045+Divyanshu23143@users.noreply.github.com>
2024-09-28 17:05:29 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function toggleMobileMenu(){
-	document.getElementById("menu").classList.toggle("active");
-}
-document.getElementById("myLink").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default anchor behavior
-
-    this.classList.toggle("clicked"); // Toggle the 'clicked' class on click
-});
